87f6c50b1beee4414a2fa92fb070ddae2c1c971c
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-01 23:31:02 +0100

    fix linkage

--- Editor/GizmoUtils.cs
+++ Editor/GizmoUtils.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public static class GizmoUtils
+namespace UniHelper.Editor
 {
-    public static void DrawPath(IEnumerable<Vector3> path, bool looped = false) =>
-        Gizmos.DrawLineStrip(path.ToArray<Vector3>(), looped);
+    public static class GizmoUtils
+    {
+        public static void DrawPath(IEnumerable<Vector3> path, bool looped = false) =>
+            Gizmos.DrawLineStrip(path.ToArray<Vector3>(), looped);
 
-    public static void DrawPath(IEnumerable<Vector2> path, bool looped = false, float zOffset = 0) =>
-        DrawPath(path.Select(p => new Vector3(p.x, p.y, zOffset)).ToArray(), looped);
+        public static void DrawPath(IEnumerable<Vector2> path, bool looped = false, float zOffset = 0) =>
+            DrawPath(path.Select(p => new Vector3(p.x, p.y, zOffset)).ToArray(), looped);
 
-    public static void DrawChildrenPath(this Transform value, bool looped = false) =>
-        DrawPath(value.Children().Select(p => p.position).ToArray(), looped);
+        public static void DrawChildrenPath(this Transform value, bool looped = false) =>
+            DrawPath(value.Children().Select(p => p.position).ToArray(), looped);
 
-    public static void DrawChildrenPath(this GameObject value, bool looped = false) =>
-        DrawChildrenPath(value.transform, looped);
+        public static void DrawChildrenPath(this GameObject value, bool looped = false) =>
+            DrawChildrenPath(value.transform, looped);
+    }
 }
